71d9e9362f9f70dcc6a7a96b210356f892d396de
Enot735 <kotiksv19@gmail.com>
2021-03-11 20:12:42 +1000

    First commit
    Тестирование Dynamic batching, Static batching и GPU Instancing для показателей FPS, Batch, Saved by batching

---
+++ Assets/Scripts/BubbleMain.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleMain : MonoBehaviour
+{
+    public GameObject myPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Instantiate(myPrefab, new Vector3(Random.Range(-2, 3), i * 5, Random.Range(-2, 3)), Quaternion.identity);
+        }        
+    }
+}
